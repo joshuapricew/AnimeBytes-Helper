d27e4b1bdc5dd15a976dddff66eb4e619c84dbdd
Joshua Price <32619882+joshuapricew@users.noreply.github.com>
2018-01-12 12:43:01 +0000

    Create main.cpp

---
+++ AnimeBytes-Helper/main.cpp
@@ -0,0 +1,26 @@
+// main.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include "constants.h"
+#include "functions.h"
+
+int main()
+{
+	std::cout << "##~~ AnimeBytes Helper ~~##\n"
+		<< '\n'
+		<< "Seeding Time Calculator\n"
+		<< "How many gigabytes is the torrent?\n";
+	double gigabytes{ getValueFromUser() };
+	std::cout << '\n';
+
+	int seedTime{ calcSeedTime((int)gigabytes) };
+	formatAndPrintSeedTime(seedTime);
+
+	int gracePeriod{ seedTime * constants::gracePeriodMultiplier };
+	formatAndPrintGracePeriod(gracePeriod);
+
+	end();
+	return 0;
+}
