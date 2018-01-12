6024daea13a8dbdb78784e8a899dd55c5448adcd
Joshua Price <32619882+joshuapricew@users.noreply.github.com>
2018-01-12 12:44:33 +0000

    Create constants.h

---
+++ AnimeBytes-Helper/constants.h
@@ -0,0 +1,10 @@
+#pragma once
+
+namespace constants
+{
+	constexpr int baseSeedHours{ 72 };
+	constexpr int baseGigabytes{ 10 };
+	constexpr int hoursPerGigabyte{ 5 };
+	constexpr int hoursInADay{ 24 };
+	constexpr int gracePeriodMultiplier{ 4 };
+}
