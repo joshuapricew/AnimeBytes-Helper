2872f4a3e849cd1212af62db39425eeda17c39fa
Joshua Price <32619882+joshuapricew@users.noreply.github.com>
2018-01-12 12:45:33 +0000

    Create functions.h

---
+++ AnimeBytes-Helper/functions.h
@@ -0,0 +1,11 @@
+#pragma once
+
+void end();
+double getValueFromUser();
+int calcSeedTime(int gigabytes);
+std::string days(int d, int h);
+void formatAndPrintSeedTime(int seedTime);
+void formatAndPrintGracePeriod(int gracePeriod);
+
+// --- AnimeBytes Helper Functions ---
+// void seedingTimeCalculator();
