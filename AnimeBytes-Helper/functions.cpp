3f0cad51e11dc535d813399cd1d7c29e09dc3378
Joshua Price <32619882+joshuapricew@users.noreply.github.com>
2018-01-24 22:45:39 +0000

    Made some optimisations
    Simplified some data types and optimised calcSeedTime() function

--- AnimeBytes-Helper/functions.cpp
+++ AnimeBytes-Helper/functions.cpp
@@ -8,29 +8,28 @@ void end()
 	std::cout << '\n' << "Press enter to continue . . . "; std::cin.clear(); std::cin.ignore(32767, '\n'); std::cin.get();
 }
 
-float getValueFromUser() 
+double getValueFromUser() 
 {
-	float gigabytes{};
+	double gigabytes{};
 	std::cin >> gigabytes;
 	return gigabytes;
 }
 
-int calcSeedTime(float gigabytes)
+int calcSeedTime(double gigabytes)
 {
 	// If torrent is under 10GB the minimum seed time is always 72 hours.
-	if (gigabytes <= 10.0f)
+	if (gigabytes <= 10.0)
 		return constants::baseSeedHours;
 	else
 	{
-		int seedTime{};
 		gigabytes -= constants::baseGigabytes;
 		gigabytes *= constants::hoursPerGigabyte;
-		return (int)gigabytes + constants::baseSeedHours;
+		return constants::baseSeedHours + static_cast<int>(gigabytes);
 	}
 }
 
 // Correctly pluralises and omits strings
-std::string days(int d, short h)
+std::string days(int d, int h)
 {
 	std::string days{ std::to_string(d) };
 	std::string hours{ std::to_string(h) };
@@ -45,17 +44,17 @@ std::string days(int d, short h)
 void formatAndPrintSeedTime(int seedTime)
 {
 	int seedDays{ seedTime / constants::hoursInADay };
-	short seedHours{ seedTime % constants::hoursInADay };
+	int seedHours{ seedTime % constants::hoursInADay };
 	std::cout << "Minimum seed time: " << days(seedDays, seedHours) << " or: " << seedTime << " hours" << '\n';
 }
 
 void formatAndPrintGracePeriod(int gracePeriod)
 {
 	int graceDays = gracePeriod / constants::hoursInADay;
-	short graceHours = gracePeriod % constants::hoursInADay;
+	int graceHours = gracePeriod % constants::hoursInADay;
 	std::cout << "Grace period: " << days(graceDays, graceHours) << " or: " << gracePeriod << " hours" << '\n';
 }
 
-// --- AnimeBytes Helper Functions ---
+/* --- AnimeBytes Helper Functions --- */
 
 // void seedingTimeCalculator() {}
